0ddf352eec45e8d99a731e9084b60da66c68da17
yjcho <jinjanic91@gmail.com>
2020-12-17 23:29:25 +0900

    로그인 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import firebase from './firebase';
+import fbase from './fbase';
 
 //check if firebase connected with this pj
 // console.log(firebase);
